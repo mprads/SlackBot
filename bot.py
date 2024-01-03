4b8b93452f5e2ecd2954257bc51d43f0d767b70d
Myles Rademaker <myles@buysellads.com>
2024-01-03 15:06:01 -0800

    Bot sends welcome message and stores when user types start

--- bot.py
+++ bot.py
@@ -6,6 +6,60 @@ from flask import Flask, Response, request
 from slackeventsapi import SlackEventAdapter
 
 message_counts = {}
+welcome_messages = {}
+
+class WelcomeMessage:
+    START_TEXT = {
+        'type': 'section',
+        'text' : {
+            'type': 'mrkdwn',
+            'text': (
+                'Welcome to the channel \n\n'
+                '*Get started by completing the tasks*'
+                )
+        }
+    }
+
+    DIVIDER = {
+        'type': 'divider'
+    }
+
+    def __init__(self, channel, user):
+        self.channel = channel
+        self.user = user
+        self.timestamp =  ''
+        self.completed = False
+
+    def get_message(self):
+        return {
+            'ts': self.timestamp,
+            'channel': self.channel,
+            'username': 'Welcome Robot',
+            'blocks': [
+                self.START_TEXT,
+                self.DIVIDER,
+                self._get_reaction_task()
+            ]
+        }
+    
+    def _get_reaction_task(self):
+        checkmark = ':white_check_mark:'
+        if not self.completed:
+            checkmark = ':white_large_square:'
+
+        text = f'{checkmark} *React to this message'
+
+        return {'type': 'section', 'text': {'type': 'mrkdwn', 'text': text}}
+
+def send_welcome_message(channel, user):
+    welcome = WelcomeMessage(channel, user)
+    message = welcome.get_message()
+    response = client.chat_postMessage(**message)
+    welcome.timestamp = response['ts']
+
+    if channel not in welcome_messages:
+        welcome_messages[channel] = {}
+    welcome_messages[channel][user] = welcome
 
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
@@ -25,13 +79,14 @@ def message(payload):
     user_id = event.get('user')
     text = event.get('text')
 
-    if user_id != BOT_ID:
+    if user_id != None and user_id != BOT_ID:
         if user_id in message_counts:
             message_counts[user_id] += 1
         else:
             message_counts[user_id] = 1
 
-        client.chat_postMessage(channel=channel_id, text=text)
+        if text.lower() == 'start':
+            send_welcome_message(channel_id, user_id)
 
 @app.route('/message-count', methods=['POST'])
 def message_count():
